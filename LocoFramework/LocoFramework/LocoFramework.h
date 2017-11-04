9ab512928ab52e84179cdcce364cd85d3cfd32d1
Kyle <kiloloco@me.com>
2017-11-03 20:35:06 -0700

    Finished our Loco framerwork

---
+++ LocoFramework/LocoFramework/LocoFramework.h
@@ -0,0 +1,19 @@
+//
+//  LocoFramework.h
+//  LocoFramework
+//
+//  Created by Kyle Lee on 11/3/17.
+//  Copyright © 2017 Kilo Loco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocoFramework.
+FOUNDATION_EXPORT double LocoFrameworkVersionNumber;
+
+//! Project version string for LocoFramework.
+FOUNDATION_EXPORT const unsigned char LocoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocoFramework/PublicHeader.h>
+
+
---
+++ LocoFramework/LocoFramework/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  LocoFramework
+//
+//  Created by Kyle Lee on 11/3/17.
+//  Copyright © 2017 Kilo Loco. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Did Some Stuff, here you go homeboi!"
+    }
+    
+}
